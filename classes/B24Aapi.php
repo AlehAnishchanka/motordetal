bc719b5432629e4b57733e525a32967ab4a4084d
Aleh Anishchanka <kit88888888@gmail.com>
2023-03-01 15:27:09 +0300

    add main api class

---
+++ classes/B24Aapi.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace  report;
+use Helper;
+
+class B24Aapi
+{
+
+    CONST WEB_HOOK = 'https://motordetal.bitrix24.ru/rest/2944/3tux3pkmwzqpz1z1/';
+
+    CONST AR_METHODS = [
+        'GET_LIST_STAGES'=>'crm.status.list',
+        //'GET_LIST_DEALS_HISTORY'=>'crm.stagehistory.list',
+        //'GET_LIST_DEALS_STAGE_BY_CATEGORY_ID'=>'crm.dealcategory.stage.list',
+        'GET_LIST_VACANCY' => 'crm.deal.list',
+    ];
+
+    protected function SendRequest($method, $arParams)
+    {
+        $queryUrl = B24Aapi::WEB_HOOK.$method;
+        $queryData = http_build_query($arParams);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_POST => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $queryUrl,
+            CURLOPT_POSTFIELDS => $queryData,
+        ));
+        $result = curl_exec($curl);
+        curl_close($curl);
+        $result = json_decode($result);
+        return $result;
+    }
+
+    public function getItemsQuantity($method, $filter='', $preset='')
+    {
+
+        $result = $this->SendRequest($method . "?$preset" . http_build_query(['filter' => $filter,'select'=>[]]), []);
+        return $result->total;
+    }
+
+    public function getArCmdItem($method, $arFilter, $arSelect, $order=["ID"=> "asc"], $preset='')
+    {
+        $cmdArr = [];
+        $preset = $preset ? "$preset&" : $preset;
+        $itemsCount = $this->getItemsQuantity($method, $arFilter, $preset);
+        $countPages = intdiv($itemsCount,50);
+        $countPages = $itemsCount%50>0 ? $countPages + 1 : $countPages;
+        for($i=0; $i<$countPages; $i++) {
+            $cmdArr[$method . '_' . $i] =
+                $method . "?$preset" .  http_build_query(
+                    [
+                        'order' => $order,
+                        'start' => $i*50,
+                        'filter' => $arFilter,
+                        'select' => $arSelect
+                    ]);
+        }
+        return $cmdArr;
+    }
+
+    public function getArBatchResult($methodApi, $arFilter=[], $arSelect=["*", "UF_*"], $order=["ID"=> "asc"], $preset='')
+    {
+        $method = 'batch';
+        $array = [
+            'halt' => 0,
+            'cmd' => $this->getArCmdItem($methodApi, $arFilter, $arSelect, $order, $preset)
+        ];
+        $arCountResult =  \Helper::object_to_array($this->SendRequest($method, $array)->result->result);
+        return $arCountResult;
+    }
+
+    public function getArSimpleResult($methodApi, $param=[], $arFilter=[])
+    {
+        if($param) {
+            $methodApi .= '?';
+            foreach($param as $key=>$val) {
+                $methodApi .= "$key=$val&";
+            }
+            $methodApi = substr($methodApi, 0 , strlen($methodApi)-1);
+        }
+        $arCountResult =  \Helper::object_to_array($this->SendRequest($methodApi, $arFilter)->result);
+        return $arCountResult;
+    }
+
+    public function getArBatchParamDependResult($methodApi, $paramName, $arParamValues)
+    {
+        $arCmd = [];
+        foreach($arParamValues as $paramValue) {
+            $arCmd[$paramValue] = "$methodApi?$paramName=$paramValue";
+        }
+        $method = 'batch';
+        $array = [
+            'halt' => 0,
+            'cmd' => $arCmd
+        ];
+        $arCountResult =  \Helper::object_to_array($this->SendRequest($method, $array)->result->result);
+        return $arCountResult;
+    }
+}
